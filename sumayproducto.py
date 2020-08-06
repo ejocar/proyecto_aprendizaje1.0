9190564d3a1dc04295788526fef51c9ecf9d4c57
Joel <ejocarluengo@gmail.com>
2020-08-06 19:48:00 +0200

    Cuarta actualización

---
+++ sumayproducto.py
@@ -0,0 +1,10 @@
+
+numero1 = input("Introduce un numero: ")
+numero2 = input("Introduce otro numero: ")
+
+resultado1 = numero1 + numero2
+resultado2 = numero1 * numero2
+
+print "resultado1","=",resultado1,"","(",numero1,"+",numero2,")"
+print "y"
+print "resultado2","=",resultado2,"","(",numero1,"*",numero2,")"

47ce51812e24c1b0b85f509f04607e62450f5b91
joelluengogutierrez <ejocar4@icloud.com>
2021-04-16 12:15:35 +0200

    Cuarta actualización

---
+++ Numero Mayor.py
@@ -0,0 +1,8 @@
+
+numero1 = input("Introduce un numero: ")
+numero2 = input("Introduce otro numero: ")
+
+if numero1 > numero2:
+    print("Este es el numero mas alto de los dos: {}".format(numero1))
+else:
+    print("Este es el numero mas alto de los dos: {}".format(numero2))
